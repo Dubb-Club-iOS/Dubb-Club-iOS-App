b1036cc0c456482aecc35f87487b965cb1c209c7
Chris Koehne <cdkoehne@gmail.com>
2021-03-22 18:35:21 -0400

    Setting up Xcode project

---
+++ DubbClubiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DubbClubiOS
+//
+//  Created by Chris Koehne on 3/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DubbClubiOS/DubbClubiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubbClubiOSApp.swift
+//  DubbClubiOS
+//
+//  Created by Chris Koehne on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct DubbClubiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
