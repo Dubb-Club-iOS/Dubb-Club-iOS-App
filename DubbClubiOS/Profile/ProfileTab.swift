f7449f33570cb776be0c78eb7001bba694376958
BrandonLC2020 <43575529+BrandonLC2020@users.noreply.github.com>
2021-03-31 18:46:57 -0400

    updated the profile tab

--- DubbClubiOS/Profile/ProfileTab.swift
+++ DubbClubiOS/Profile/ProfileTab.swift
@@ -11,28 +11,27 @@ struct ProfileTab: View {
     var body: some View {
         NavigationView {
             /*
-            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
-            */
+             NavigationLink(destination: Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate") }
+             */
             ScrollView {
                 VStack {
                     /*
-                    HStack{
-                        Text("Profile")
-                            .font(.system(size: 34))
-                            .fontWeight(.heavy)
-                            .padding([.top, .leading], 10.0)
-                        Spacer()
-                    }
-                    */
+                     HStack{
+                     Text("Profile")
+                     .font(.system(size: 34))
+                     .fontWeight(.heavy)
+                     .padding([.top, .leading], 10.0)
+                     Spacer()
+                     }
+                     */
                     Image("DubbClub Logo")
                         .resizable()
-                        .padding(.all)
-                        .frame(width: 865/4, height: 868/4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .frame(width: 865/5, height: 868/5, alignment: .center)
                     Text("@exampleUsername")
                         .font(.system(size: 22))
                         .fontWeight(.bold)
                         .padding(.all, 20)
-                        .frame(width: 210, height: 90)
+                        .frame(width: 300, height: 50)
                     HStack {
                         Text("Teams Following:")
                             .fontWeight(.semibold)
