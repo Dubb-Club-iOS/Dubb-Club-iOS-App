8a923b17119ad810b19519deed043d7413cf73f7
BrandonLC2020 <43575529+BrandonLC2020@users.noreply.github.com>
2021-03-31 18:45:03 -0400

    added scroll view and other things to the profile tab

--- DubbClubiOS/Profile/ProfileTab.swift
+++ DubbClubiOS/Profile/ProfileTab.swift
@@ -9,12 +9,41 @@ import SwiftUI
 
 struct ProfileTab: View {
     var body: some View {
-        VStack {
-            Text("@exampleUsername")
-                .fontWeight(.bold)
-            Spacer()
-            Text("Teams Following:")
-            TeamsFollowingList()
+        NavigationView {
+            /*
+            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
+            */
+            ScrollView {
+                VStack {
+                    /*
+                    HStack{
+                        Text("Profile")
+                            .font(.system(size: 34))
+                            .fontWeight(.heavy)
+                            .padding([.top, .leading], 10.0)
+                        Spacer()
+                    }
+                    */
+                    Image("DubbClub Logo")
+                        .resizable()
+                        .padding(.all)
+                        .frame(width: 865/4, height: 868/4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Text("@exampleUsername")
+                        .font(.system(size: 22))
+                        .fontWeight(.bold)
+                        .padding(.all, 20)
+                        .frame(width: 210, height: 90)
+                    HStack {
+                        Text("Teams Following:")
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.leading)
+                            .padding(.leading, 10)
+                        Spacer()
+                    }
+                    TeamsFollowingList()
+                }
+            }
+            .navigationTitle("Profile")
         }
     }
 }
