7b78636f7cec387cb216000747e3f8bd1425b159
BrandonLC2020 <43575529+BrandonLC2020@users.noreply.github.com>
2021-04-05 18:27:28 -0400

    updated background color for profile

--- DubbClubiOS/Profile/ProfileTab.swift
+++ DubbClubiOS/Profile/ProfileTab.swift
@@ -15,43 +15,46 @@ struct ProfileTab: View {
             /*
              NavigationLink(destination: Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate") }
              */
-            ScrollView {
-                VStack {
-                    /*
-                     HStack{
-                     Text("Profile")
-                     .font(.system(size: 34))
-                     .fontWeight(.heavy)
-                     .padding([.top, .leading], 10.0)
-                     Spacer()
-                     }
-                     */
-                    Image("DubbClub Logo")
-                        .resizable()
-                        .frame(width: 865/5, height: 868/5, alignment: .center)
-                    Text("@exampleUsername")
-                        .font(.system(size: 22))
-                        .fontWeight(.bold)
-                        .padding(.all, 20)
-                        .frame(width: 300, height: 50)
-                    HStack {
-                        Text("Teams Following:")
-                            .fontWeight(.semibold)
-                            .multilineTextAlignment(.leading)
-                            .padding(.leading, 10)
-                        Spacer()
+            ZStack {
+                ColorManager.backgroundGray.ignoresSafeArea()
+                ScrollView {
+                    VStack {
+                        /*
+                         HStack{
+                         Text("Profile")
+                         .font(.system(size: 34))
+                         .fontWeight(.heavy)
+                         .padding([.top, .leading], 10.0)
+                         Spacer()
+                         }
+                         */
+                        Image("DubbClub Logo")
+                            .resizable()
+                            .frame(width: 865/5, height: 868/5, alignment: .center)
+                        Text("@exampleUsername")
+                            .font(.system(size: 22))
+                            .fontWeight(.bold)
+                            .padding(.all, 20)
+                            .frame(width: 300, height: 50)
+                        HStack {
+                            Text("Teams Following:")
+                                .fontWeight(.semibold)
+                                .multilineTextAlignment(.leading)
+                                .padding(.leading, 10)
+                            Spacer()
+                        }
+                        TeamsFollowingList()
+                        NavigationLink(destination: LoginUIView()) {
+                            Text("Log Out")
+                        }.simultaneousGesture(TapGesture().onEnded{
+                            self.logout()
+                        })
                     }
-                    TeamsFollowingList()
-                    NavigationLink(destination: LoginUIView()) {
-                        Text("Log Out")
-                    }.simultaneousGesture(TapGesture().onEnded{
-                        self.logout()
-                    })
+                    
+                    
                 }
-                
-                
+                .navigationTitle("Profile")
             }
-            .navigationTitle("Profile")
         }
     }
     func logout() {
