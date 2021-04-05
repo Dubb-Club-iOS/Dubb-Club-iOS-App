ef26e4a3967556999bbec67031971adcb6c918cc
Aditya Naik <adityan9900@gmail.com>
2021-04-05 00:09:59 -0400

    Implemented login, registration and logout logic

--- DubbClubiOS/LoginAndRegister/LoginUIView.swift
+++ DubbClubiOS/LoginAndRegister/LoginUIView.swift
@@ -11,6 +11,92 @@ struct LoginUIView: View {
     @State private var email = ""
     @State private var password = ""
     @State private var showSignUp = false
+    @State var isLoggedIn = false
+    
+    // TODO: use elsewhere
+    func getFavoriteTeams() {
+        let url = URL(string: "https://api.dubb.club/api/user/favoriteteamlist")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        request.addValue(UserDefaults.standard.object(forKey: "JWT") as! String, forHTTPHeaderField: "x-access-token")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 404 {
+                    print("Invalid login!")
+                    return
+                } else if httpResponse.statusCode == 500 {
+                    print("Database failure!")
+                    return
+                }
+            }
+            
+            
+            if let error = error {
+                // Handle HTTP request error
+                print("Error: \(error.localizedDescription)")
+            } else if let data = data {
+                // Handle HTTP request response
+                let ret: FavoriteTeamsParent = try! JSONDecoder().decode(FavoriteTeamsParent.self, from: data)
+                print("Fav Teams: ")
+                for team in ret.favoriteTeams.NBA {
+                    print(team)
+                }
+            } else {
+                print("Unexpected error!")
+            }
+        }.resume()
+    }
+    
+    func login() {
+        
+        let token = UserDefaults.standard.object(forKey: "JWT") as? String
+        if token != nil {
+            getFavoriteTeams()
+            return
+        }
+        
+        let loginInfo = LoginInfo(username: self.email, password: self.password)
+        guard let loginEnc = try? JSONEncoder().encode(loginInfo) else {
+            print("Failed to encode login information")
+            return
+        }
+        
+        let url = URL(string: "https://api.dubb.club/api/auth/login")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = loginEnc
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 404 {
+                    print("Invalid login!")
+                    return
+                } else if httpResponse.statusCode == 500 {
+                    print("Database failure!")
+                    return
+                }
+            }
+            
+            
+            if let error = error {
+                // Handle HTTP request error
+                print("Error: \(error.localizedDescription)")
+            } else if let data = data {
+                // Handle HTTP request response
+                let loginReturn: LoginReturn = try! JSONDecoder().decode(LoginReturn.self, from: data)
+                print(loginReturn.username)
+                UserDefaults.standard.set(loginReturn.accessToken, forKey:"JWT")
+                self.isLoggedIn = true
+            } else {
+                print("Unexpected error!")
+            }
+        }.resume()
+    }
     
     var body: some View {
         NavigationView {
@@ -31,7 +117,7 @@ struct LoginUIView: View {
                         .padding(.bottom)
                     VStack(spacing: 0){
                         
-                        TextField(" Email", text: self.$email)
+                        TextField("", text: self.$email)
                             .modifier(PlaceholderStyle(showPlaceHolder: email.isEmpty, placeholder: "Username or Email"))
                             .foregroundColor(.gray)
                             .frame(width: 400.0, height: 50.0)
@@ -41,7 +127,7 @@ struct LoginUIView: View {
                         
                         Divider().background(Color.gray).frame(width: 400)
                         HStack(){
-                            SecureField(" Password", text: self.$password)
+                            SecureField("Password", text: self.$password)
                                 .modifier(PlaceholderStyle(showPlaceHolder: password.isEmpty, placeholder: "Password"))
                                 .foregroundColor(.gray)
                                 .frame(width: 250.0, height: 50.0)
@@ -63,22 +149,21 @@ struct LoginUIView: View {
                         Divider().background(Color.gray).frame(width: 400, height: 0, alignment: .center)
                     }
                     
-                    
-                    Button(action: {}, label: {
-                        Text("Sign In")
-                            .font(.headline)
-                            .fontWeight(.regular)
-                            .foregroundColor(Color.white)
-                            .frame(width: 200, height: 50, alignment: .center)
-                            .background(Color.blue)
-                            .cornerRadius(12.0)
-                            .padding(.top, 200.0)
-                            .onTapGesture(perform: {
-                                showSignUp = true
-                            })
-                        
-                        
-                    }).padding(.bottom, 10.0)
+                    NavigationLink(destination: ProfileTab(), isActive: $isLoggedIn) {
+                        Button(action: {}, label: {
+                            Text("Sign In")
+                                .font(.headline)
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.white)
+                                .frame(width: 200, height: 50, alignment: .center)
+                                .background(Color.blue)
+                                .cornerRadius(12.0)
+                                .onTapGesture(perform: {
+                                    showSignUp = true
+                                    self.login()
+                                })
+                        }).padding(.bottom, 10.0)
+                    }.padding(.top, 200.0)
                     
                     
                     HStack(spacing: 4){
@@ -92,7 +177,7 @@ struct LoginUIView: View {
                     }
                 }
             }
-        }
+        }.navigationBarHidden(true)
     }
 }
 
