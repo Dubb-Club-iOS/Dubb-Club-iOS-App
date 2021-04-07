60c84b4f615344e5001cfcf78d528a18ada4b792
Aditya Naik <adityan9900@gmail.com>
2021-04-07 18:33:04 -0400

    Implemented refresh of JWT

--- DubbClubiOS/DubbClubiOSApp.swift
+++ DubbClubiOS/DubbClubiOSApp.swift
@@ -9,10 +9,54 @@ import SwiftUI
 
 @main
 struct DubbClubiOSApp: App {
+    
+    init() {
+        refreshToken()
+    }
+    
+    func refreshToken() {
+        
+        let token = UserDefaults.standard.object(forKey: "JWT") as? String
+        if token == nil {
+            print("No user logged in!")
+            return
+        }
+        
+        let url = URL(string: "https://api.dubb.club/api/auth/refresh")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.addValue(UserDefaults.standard.object(forKey: "JWT") as! String, forHTTPHeaderField: "x-access-token")
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                
+                if httpResponse.statusCode != 200 {
+                    print("User token expired!")
+                    UserDefaults.standard.set(nil, forKey:"JWT")
+                    UserDefaults.standard.set(nil, forKey:"Username")
+                    return
+                }
+            }
+            
+            if let error = error {
+                // Handle HTTP request error
+                print("Error: \(error.localizedDescription)")
+            } else if let data = data {
+                print("Successful refresh of JWT!")
+                let refreshReturn: RefreshReturn = try! JSONDecoder().decode(RefreshReturn.self, from: data)
+                UserDefaults.standard.set(refreshReturn.accessToken, forKey:"JWT")
+            } else {
+                print("Unexpected error!")
+            }
+        }.resume()
+    }
+    
     var body: some Scene {
         WindowGroup {
 //            HomeStream()
-            LoginUIView()
+//            LoginUIView()
+            ProfileTab()
         }
     }
 }
