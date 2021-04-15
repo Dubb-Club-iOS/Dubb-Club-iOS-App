f4479fb08c4a17341b92c9bcfacf9a0a09a11594
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2021-04-15 13:23:41 -0400

    added logout button

--- DubbClubiOS/Search/SearchBarAnimationUtility.swift
+++ DubbClubiOS/Search/SearchBarAnimationUtility.swift
@@ -3,7 +3,6 @@
 //  DubbClubiOS
 //
 //  Created by Brandon Lamer-Connolly on 4/14/21.
-//  Code and implementation found on https://www.avanderlee.com/swiftui/withanimation-completion-callback/
 
 
 import Foundation
